113127f01049932ac019d78d5dc438d872739a53
cstrikest <cstrikest@gmail.com>
2023-04-27 11:06:45 +0900

    继承，虚函数。进度Primer Plus 13.6。

--- cppPlayground/stack.cpp
+++ cppPlayground/stack.cpp
@@ -3,8 +3,8 @@
 // 复制构造函数 需要提供新new的指针。因为旧的已经被delete，直接赋值会指向释放过的内存。
 Stack::Stack(const Stack& stack)
 {
-	__maxPtr = stack.__maxPtr;
-	__mem = new int[__maxPtr];
+	__maxptr = stack.__maxptr;
+	__mem = new int[__maxptr];
 	*__mem = *stack.__mem;
 	__ptr = stack.__ptr;
 }
@@ -15,7 +15,15 @@ Stack::Stack(const Stack& stack)
 
 void Stack::showStack(std::ostream& cout) const
 {
-	for (int i = __ptr - 1; i > 0 - 1; i--)
+	for (int i = __ptr - 1; i >= 0; i--)
+	{
+		cout << std::hex << i * 4 << "\t\t" << std::dec << *(__mem + i) << std::endl;
+	}
+}
+
+void ZeroableStack::showStack(std::ostream& cout) const
+{
+	for (int i = __maxptr - 1; i >= 0; i--)
 	{
 		cout << std::hex << i * 4 << "\t\t" << std::dec << *(__mem + i) << std::endl;
 	}
@@ -24,34 +32,34 @@ void Stack::showStack(std::ostream& cout) const
 // 运算符重载
 Stack& Stack::operator+(int value)
 {
-	if (this->__ptr < this->__maxPtr) *((this->__mem) + (this->__ptr)++) = value;
+	if (__ptr < __maxptr) *((__mem) + (__ptr)++) = value;
 	return *this;
 }
 
 Stack& Stack::operator+=(int value)
 {
-	if (this->__ptr < this->__maxPtr) *((this->__mem) + (this->__ptr)++) = value;
+	if (__ptr < __maxptr) *((__mem) + (__ptr)++) = value;
 	return *this;
 }
 
 int Stack::operator--() { if (__ptr > 0) return *(__mem + --__ptr); }
 
 // 友元运算符重载 
-std::ostream& operator<<(std::ostream& os, Stack& stack) { return os << "A stack with " << stack.__maxPtr << " integer."; }
+std::ostream& operator<<(std::ostream& os, Stack& stack) { return os << "A stack with " << stack.__maxptr << " integer."; }
 
 // 赋值运算符重载 判断是不是自赋值，然后delete掉等号左边对象现有的指针，new新的指针再赋值。
 Stack& Stack::operator=(const Stack& stack)
 {
 	if (this == &stack) return *this;
 	delete[] __mem;
-	__mem = new int[__maxPtr];
-	__maxPtr = stack.__maxPtr;
+	__mem = new int[__maxptr];
+	__maxptr = stack.__maxptr;
 	*__mem = *stack.__mem;
 	__ptr = stack.__ptr;
 	return *this;
 }
 
-Stack::operator int* () { return this->__mem; }
+Stack::operator int* () { return __mem; }
 
 void runStack()
 {
@@ -69,7 +77,9 @@ void runStack()
 	std::cout << "Maxium " << 4 * std::stoi(size) << " bytes." << std::endl;
 	while (true)
 	{
-		std::cout << ">>";
+		if (stack.isFull()) std::cout << ">|";
+		else if (stack.isEmpty()) std::cout << "|>";
+		else std::cout << ">>";
 		std::getline(std::cin, tmp);
 		std::cin.clear();
 		if (isNumeric(tmp))
@@ -93,8 +103,9 @@ void runStack()
 		}
 		else if (tmp[0] == 's')
 		{
-			if (stack.isEmpty()) std::cout << "-\t\t-" << std::endl;
-			else stack.showStack(std::cout);
+			//if (stack.isEmpty()) std::cout << "-\t\t-" << std::endl;
+			//else stack.showStack(std::cout);
+			stack.showStack(std::cout);
 		}
 		else if (tmp[0] == 'q' or tmp[0] == 'Q') break;
 		else
--- cppPlayground/stack.h
+++ cppPlayground/stack.h
@@ -1,34 +1,39 @@
-#pragma once
+﻿#pragma once
 #include"utils.h"
 
 class Stack
 {
-private:
+protected:
 	int __ptr;
-	int __maxPtr;
+	int __maxptr;
 	int* __mem;
 
 public:
-	inline Stack(int size)
-	{
-		__maxPtr = size;
-		__mem = new int[__maxPtr];
-		__ptr = 0;
-	}
+	// 内联函数必须定义在头文件。因为编译器需要在随时展开。
+	// 所以在单个编译单元内必须能找到定义。
+	// 每个文件内都定义一次不现实，而且定义不同的话会出现无意义展开。
+	// 因此推荐在头文件内定义内联函数。
+	// 之前因为这个找了半天问题。还以为是vs的锅。
+
+	// 类构造函数初始化列表
+	// 直接用复制构造函数初始化，不用先调用默认构造函数再调用赋值运算符的重载。
+	// 如果子类不指定要使用的基类构造函数，则调用默认构造函数。
+	// 这里的话跟以下函数Stack()的重载匹配。
+	inline Stack(int size = 0) :
+		__ptr(0), __maxptr(size), __mem(new int[__maxptr]) {}
+
 	Stack(const Stack&);
-	inline ~Stack() { delete[] __mem; }
-	
-	inline void push(int value) { if (__ptr < __maxPtr) *((__mem)+__ptr++) = value; }
-
+	virtual inline ~Stack() { delete[] __mem; std::cout << "!!!"; }
+	inline void push(int value)
+	{
+		if (__ptr < __maxptr) *((__mem)+__ptr++) = value;
+	}
 	inline int pop() { if (__ptr > 0) return *(__mem + --__ptr); }
-
 	inline int getsize() const { return __ptr; }
-
-	inline bool isFull() const { return __ptr == __maxPtr; }
-
+	inline bool isFull() const { return __ptr == __maxptr; }
 	inline bool isEmpty() const { return __ptr == 0; };
-	
-	void showStack(std::ostream&) const;
+
+	virtual void showStack(std::ostream&) const;
 
 	Stack& operator+(int);
 	Stack& operator+=(int);
@@ -40,4 +45,20 @@ public:
 	operator int* ();
 };
 
+class ZeroableStack :public Stack
+{
+private:
+	int __zeronum;
+public:
+	// 子类的构造函数初始化列表，调用了基类的构造函数。
+	inline ZeroableStack(int zeronum, int size) :
+		Stack(size), __zeronum(zeronum) {}
+
+	// 虚析构函数，确保释放派生对象时保持正常的释放顺序。
+	inline virtual ~ZeroableStack() {}
+
+	virtual void showStack(std::ostream&) const;
+
+
+};
 void runStack();
